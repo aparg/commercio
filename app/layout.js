e303d2ab36f0540a75aa2799cb2d0b89c50d44cc
aparg <apargtm@gmail.com>
2025-04-02 20:59:18 +0545

    fix: remove popup

--- app/layout.js
+++ app/layout.js
@@ -135,7 +135,6 @@ export default function RootLayout({ children }) {
         <FloatingContact />
         <NewsLetter />
         <Footer />
-        <CommunityPopup />
       </body>
     </html>
   );
